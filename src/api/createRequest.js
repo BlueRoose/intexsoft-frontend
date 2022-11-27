81769d0c3b783da0df4eb55e64b0747e3d4bd215
Egor Zhuk <royalecool460@gmail.com>
2022-11-27 23:05:52 +0300

    change baseURL for deploy

--- src/api/createRequest.js
+++ src/api/createRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getSessionFromStorage } from "../helpers/tokens";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://intexgrambackend.herokuapp.com",
 });
 
 export const request = async ({ headers = {}, method, url, data, params }) => {
